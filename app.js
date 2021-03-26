8623169b8d9a7ad0948360503b518d086cff5d56
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2021-03-26 02:20:38 +0100

    Ajout fonction pour mettre à jour les horloges en même temps + mise à jour des objectifs

--- app.js
+++ app.js
@@ -1,54 +1,51 @@
 let isStandardTime = true;
+let clocks = []
 
-
+/* Constructeur d'objets Clock */
 class clock {
   constructor() { 
-      const listBox = document.getElementById('list-box');
-      this.gmt = document.getElementById('list-box').value;
-      this.time = new Date(`${new Date().toUTCString()}${this.gmt}`);
-      this.container = document.createElement('div');
-      this.container.classList.add('clock');
-      const divClock = document.getElementById('divClock');
-      divClock.appendChild(this.container);
-      this.container.innerHTML = this.time.toLocaleTimeString();
-
-  }
-// WIP
-  updateClock(){
-    setInterval(() => {
-      this.time = new Date(`${new Date().toUTCString()}${document.getElementById('list-box').value}`);
-      this.container.innerHTML = this.time.toLocaleTimeString();
-    }, 1000);
+    this.gmt = document.getElementById('list-box').value;
+    this.time = new Date(`${new Date().toUTCString()}${this.gmt}`);
+    clocks.push(this);
+    this.container = document.createElement('div');
+    this.container.classList.add('clock');
+    const divClock = document.getElementById('divClock');
+    divClock.appendChild(this.container);
+    this.container.innerHTML = this.time.toLocaleTimeString();
   }
 }
 
+/* Met à jour l'heure des horloges simultanément */
+const updateClock = () => {
+  clocks.forEach((item) => {
+    item.time = new Date(`${new Date().toUTCString()}${item.gmt}`);
+    item.container.innerHTML = item.time.toLocaleTimeString();
+  });
+};
+
+/* Ajoute une nouvelle horloge lorsqu'on clique sur le bouton d'ajout */
 const addClock = () => {
   new clock();
 }
 
-// const initTime = () => {
-//   const clock = document.getElementById('clock');
-//   // const listBox = document.getElementById('list-box');
-//   // const date = new Date();
-//   // let time = new Date(`${date.toUTCString()}${listBox.value}`);
+/* Change l'heure de la première horloge lorsqu'on sélectionne un autre fuseau horaire. */
+document.getElementById('list-box').onchange = () => {
+  clocks[0].gmt = document.getElementById('list-box').value;
+  updateClock();
+}
 
-  
-//   if (isStandardTime) {
-//     clock.innerHTML = time.toLocaleTimeString();
-//   } else {
-//     ampmFunc(time);
-//   }
-// }
-
+/* Change le booléen isStandardTime au clic du bouton switchButton */
 const switchButton = () => {
   isStandardTime = !isStandardTime;
-  // initTime();
+  if (isStandardTime) {
+    updateClock();
+  } else {
+    ampmFunc(time);
+    console.log("time : " + time);
+  }
 }
 
-document.getElementById('list-box').onchange = () => {
-  // initTime();
-}
-
+// WIP 
 const ampmFunc = (time) => {
   const clock = document.getElementById('clock');
   let heures = time.getHours();
@@ -63,16 +60,8 @@ const ampmFunc = (time) => {
   }
 }
 
+/* Lance la mise à jour des horloges toutes les 1s */
+setInterval(updateClock, 1000);
 
-// setInterval(() => new clock(), 1000);
+/* Création d'une horloge au lancement de la page */
 new clock();
-
-/* To-do list :
-- option AM/PM. done
-- les heures d'autres pays. done
-- ajouter autant d'horloge que le client veut.
-- horloge qui se met à jour en même temps.
-- http server.
-- commenter et push git.
-*/
-

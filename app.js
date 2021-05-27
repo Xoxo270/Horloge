2a15feb7e97c1861aa3c0a9fd4a7f71600a91c67
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2021-05-27 17:14:39 +0200

    deleted useless code

--- app.js
+++ app.js
@@ -126,46 +126,6 @@ backgroundChange();
 
 /* Drag & Drop function */
 
-// function handleDragStart(e) {
-//   this.style.opacity = '0.4';
-// }
-
-// function handleDragEnd(e) {
-//   this.style.opacity = '1';
-//   items.forEach(function (item) {
-//     item.classList.remove('over');
-//   });
-// }
-
-// function handleDragOver(e) {
-//   if (e.preventDefault) {
-//     e.preventDefault();
-//   }
-
-//   return false;
-// }
-
-// function handleDragEnter(e) {
-//   this.classList.add('over');
-// }
-
-// function handleDragLeave(e) {
-//   this.classList.remove('over');
-// }
-
-// function handleDrop(e) {
-//   e.stopPropagation();
-//   return false;
-// }
-
-// let items = document.querySelectorAll('.divClock .clock');
-// items.forEach(function(item) {
-//   item.addEventListener('dragstart', handleDragStart, false);
-//   item.addEventListener('dragend', handleDragEnd, false);
-//   item.addEventListener('dragenter', handleDragEnter, false);
-//   item.addEventListener('dragleave', handleDragLeave, false);
-//   item.addEventListener('dragend', handleDragEnd, false);
-// });
 document.addEventListener('DOMContentLoaded', () => {
   let dragSrcEl = null;
   
